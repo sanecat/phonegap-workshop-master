17831e7fe26fb6fe062f5afb43b143dcdd9cb64f
sanecat <sanesail@gmail.com>
2013-03-14 00:10:37 +0800

    view class

--- js/main.js
+++ js/main.js
@@ -1,27 +1,11 @@
 var app = {
 
-    findByName: function() {
-        var self = this;
-        this.store.findByName($('.search-key').val(), function (employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
-    
-    renderHomeView: function() {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-
     initialize: function() {
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-    
         var self = this;
         this.store = new MemoryStore(function() {
             self.renderHomeView();
         });
     }
-
 };
 
 app.initialize();
