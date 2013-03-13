b37527a48867677cde2d37f9769562e033b0e8fb
sanecat <sanesail@gmail.com>
2013-03-13 23:55:49 +0800

    template: modify.
    complite template first.

--- js/main.js
+++ js/main.js
@@ -1,33 +1,25 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
         var self = this;
         this.store.findByName($('.search-key').val(), function (employees) {
             $('.employee-list').html(self.employeeLiTpl(employees));
         });
     },
     
-    showAlert: function (message, title) {
-        if (navigator.notification) {
-            navigator.notification.alert(message, null, title, 'OK');
-        } else {
-            alert(title ? (title + ": " + message) : message);
-        }
-    },
-
     renderHomeView: function() {
         $('body').html(this.homeTpl());
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
     initialize: function() {
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+    
         var self = this;
         this.store = new MemoryStore(function() {
             self.renderHomeView();
         });
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
     }
 
 };
