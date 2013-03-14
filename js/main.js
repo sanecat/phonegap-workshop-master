2d2e1950c50eccf89adbba7ad0bd3f3a4d6a5aa2
sanecat <sanesail@gmail.com>
2013-03-14 14:25:55 +0800

    highlighting UI element

--- js/main.js
+++ js/main.js
@@ -1,10 +1,34 @@
 var app = {
 
-    initialize: function() {
+    registerEvents: function () {
         var self = this;
-        this.store = new MemoryStore(function() {
+        // Check of browser supports touch events...
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            // ... if yes: register touch event listener to change the "selected" state of the item
+            $('body').on('touchstart', 'a', function (event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function (event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            // ... if not: register mouse events instead
+            $('body').on('mousedown', 'a', function (event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function (event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        }
+    },
+
+
+    initialize: function () {
+        var self = this;
+        this.store = new MemoryStore(function () {
             $('body').html(new HomeView(self.store).render().el);
         });
+        registerEvents();
     }
 };
 
